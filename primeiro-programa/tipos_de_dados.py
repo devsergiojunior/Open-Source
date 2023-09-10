f4e5c4361f0fecf22d416c4031631f05510615fb
Sergio Junior <dev.sergiojunior@gmail.com>
2023-09-10 02:55:59 -0300

    Create file tipo de dados

---
+++ primeiro-programa/tipos_de_dados.py
@@ -0,0 +1,29 @@
+# Tipo de Dados INT int()
+print(1 + 10)
+print(2 - 1)
+print(4 * 10)
+print(10 // 2)
+print(10 / 2)
+
+# Tipo de Dados Float float()
+print(1.4 + 10)
+print(2 - 1.8)
+print(4.3 * 10.8)
+print(10.5 // 2)
+print(10.5 / 2)
+
+# Tipo de Dados Boloean bool()
+
+print(True)
+print(False)
+print(0)
+print(1)
+
+# Tipo de Dados String str()
+
+print("'Python'")
+
+int()
+float()
+bool()
+str()
