fdcb06bba7297ae1021bbee62f0759a4b2cfa035
Sergio Junior <dev.sergiojunior@gmail.com>
2023-09-12 13:19:01 -0300

    Feat: add file variaveis e constantes

---
+++ primeiro-programa/variaveis_contantes.py
@@ -0,0 +1,14 @@
+nome = 'Sergio'
+idade = 44
+
+nome = 'Junior'
+
+nome, idade = 'Sergio Junior', 40
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ['SP','RJ','CE', 'DF','RN']
+
+print(nome,idade,limite_saque_diario,BRAZILIAN_STATES)
