2512a0ae57dfd6bfcc3ef052f31ddcee13a976e2
Sergio Junior <dev.sergiojunior@gmail.com>
2023-09-13 13:27:29 -0300

    feat: add file convertendo tipos

---
+++ primeiro-programa/convertendo_tipos.py
@@ -0,0 +1,18 @@
+# Convertendo um FLOAT para INT
+print(int(1.9))
+
+# Convertendo uma STRING para INT
+print(int("10"))
+
+# Convertendo um STRING para FLOAT
+print(float("10.10"))
+
+# Convertendo um INT em FLOAT
+print(float(100))
+
+# Convertendo um FLOAT em STR e trabalhando com variaveis.
+valor = 10
+valor_str = str(valor)
+texto = f"Esse valor {valor} é {type(valor)}, mas foi convertido em {type(valor_str)} "
+print(texto)
+      
