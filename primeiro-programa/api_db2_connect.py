3476cc5a974881f606903cd007d87a1159aad050
Sergio Junior <dev.sergiojunior@gmail.com>
2023-09-21 23:26:41 -0300

    lote de alterações

---
+++ primeiro-programa/api_db2_connect.py
@@ -0,0 +1,3 @@
+import ibm_db
+ibm_db.connect("DATABASE=CISSERP;HOSTNAME=129.148.39.208;PORT=50005;PROTOCOL=TCPIP;UID=dba;
+                PWD=a9d9p8.E10;", "", "")
---
+++ primeiro-programa/operador_logico.py
@@ -0,0 +1,10 @@
+saldo = 1000
+saque = 200
+limite = 100
+conta_especial = True
+
+print(saldo >= saque and saque <= limite) or print(conta_especial and saldo >= saque)
+
+
+print(saldo >= saque or saque <= limite)
+
