dfa4a498bef45d1823481d4eb79844c4c1f99dde
Sergio Junior <dev.sergiojunior@gmail.com>
2023-09-21 23:37:02 -0300

    update

--- primeiro-programa/operador_logico.py
+++ primeiro-programa/operador_logico.py
@@ -1,3 +1,9 @@
+
+print(True and True)
+print(True and False)
+print(True or True)
+print(True or False)
+
 saldo = 1000
 saque = 200
 limite = 100
@@ -8,3 +14,8 @@ print(saldo >= saque and saque <= limite) or print(conta_especial and saldo >= s
 
 print(saldo >= saque or saque <= limite)
 
+
+print(True and True)
+print(True and False)
+print(True or True)
+print(True or False)
