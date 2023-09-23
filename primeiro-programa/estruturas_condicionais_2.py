37f8b26610224c02fffa2a187cf6d0492af3ed65
Sergio Junior <dev.sergiojunior@gmail.com>
2023-09-23 19:39:02 -0300

    update file estrutura condicional 2

--- primeiro-programa/estruturas_condicionais_2.py
+++ primeiro-programa/estruturas_condicionais_2.py
@@ -1,21 +1,26 @@
 saldo = 2000
 cheque_especial = 450
 opcao = int(input("Informa o tipo de conta bancaria: \n[1] - Conta Corrente \n[2] - Conta Universitaria \nTipo de Conta: "))
+
+if opcao not in (1,2):
+    print("Tipo de conta invalida, sistema sera encerrado.")
 """
 conta_normal = False
 conta_universitaria = True
 """
-saque = float(input("Informa o valor para saque: R$ "))
-
 if opcao == 1:
-    if saldo >= saque:
+    saque_cc = float(input("Informa o valor para saque: R$ "))
+    if saldo >= saque_cc:
         print("Saque realizado com sucesso!")
-    elif saque <= (saldo + cheque_especial):
+    elif saque_cc <= (saldo + cheque_especial):
         print("Saque realizado com uso do cheque especial!")
     else:
         print("Não foi possivel realizar o saque, saldo insuficiente!")
 elif opcao == 2:
-    if saldo >= saque:
+    saque_cu = float(input("Informa o valor para saque: R$ "))
+    if saldo >= saque_cu:
         print("Saque realizado com sucesso")
     else:
         print("Saldo insuficiente!")
+else:
+    SystemExit
